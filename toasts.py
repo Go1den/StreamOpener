848cb83918ad229360ca0ec5ec0b8e63178e6b29
Go1den <gooneden@gmail.com>
2020-07-08 17:21:21 -0500

    Toasty

--- toasts.py
+++ toasts.py
@@ -3,5 +3,8 @@ from win10toast import ToastNotifier
 
 def toast():
     if platform.release() == "10":
-        toaster = ToastNotifier()
-        toaster.show_toast("New live streams!", "andy, cleartonic, and IvanGPX", threaded=True, icon_path='streamopenericon.ico', duration=8)
+        print("yay")
+    toaster = ToastNotifier()
+    toaster.show_toast("New live streams!", "andy, cleartonic, and IvanGPX", threaded=True, icon_path='streamopenericon.ico', duration=8)
+
+toast()
