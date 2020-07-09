4c991aa4982549be1abd9a63f2b7f2fff96fcd7b
Golden <gooneden@gmail.com>
2020-07-09 02:02:47 -0500

    Window it up

--- constants.py
+++ constants.py
@@ -2,6 +2,8 @@ from collections import OrderedDict
 
 CLIENT_ID = "dzdcecm2nwr4j6ywb349cyvv81i9sv"
 
+STREAMOPENER_ICON = 'streamopenericon.ico'
+
 TWITCH_VALIDATE_LINK = "https://id.twitch.tv/oauth2/validate"
 TWITCH_OAUTH_LINK = "https://id.twitch.tv/oauth2/authorize"
 TWITCH_LIVE_FOLLOWED_LINK = "https://api.twitch.tv/kraken/streams/followed"
@@ -24,3 +26,6 @@ TITLE_SELECT_STREAMS = "Select Streams"
 
 MSG_SELECT_SITE = "Choose the website you want to watch these streams on:"
 TITLE_SELECT_SITE = "Select Website"
+
+LABEL_STREAM_DROPDOWN = "Open streams on:"
+LABEL_STREAMOPENER = "StreamOpener"
---
+++ window.py
@@ -0,0 +1,39 @@
+from tkinter import StringVar, Tk, Frame, Label, EW, E, W, NSEW, RIGHT, CENTER, Listbox, MULTIPLE, END, Scrollbar, Y, NS
+from tkinter.ttk import Combobox
+
+from constants import STREAMOPENER_ICON, ORDERED_STREAMING_SITES, LABEL_STREAM_DROPDOWN, LABEL_STREAMOPENER
+
+class Window:
+    def __init__(self, streams=None):
+        if streams is None:
+            streams = []
+        self.window = Tk()
+        self.site = StringVar()
+        self.streams = streams
+
+        self.initializeWindow()
+        self.addListbox()
+        self.addFrame()
+
+    def initializeWindow(self):
+        self.window.iconbitmap(STREAMOPENER_ICON)
+        self.window.geometry('300x600')
+        self.window.title(LABEL_STREAMOPENER)
+
+    def addListbox(self):
+        scrollbar = Scrollbar(self.window)
+        scrollbar.grid(sticky=NS)
+        listbox = Listbox(self.window, selectmode=MULTIPLE, yscrollcommand=scrollbar)
+        scrollbar.config(command=listbox.yview)
+        for stream in self.streams:
+            listbox.insert(END, stream.split(" ")[0])
+        listbox.grid(row=0, column=0, columnspan=2, sticky=NSEW, padx=4, pady=4)
+
+    def addFrame(self):
+        labelSiteDropdown = Label(self.window, text=LABEL_STREAM_DROPDOWN)
+        labelSiteDropdown.grid(row=1, column=0, sticky=NSEW, padx=4, pady=4)
+        siteDropdown = Combobox(self.window, textvariable=self.site, state="readonly", values=list(ORDERED_STREAMING_SITES.keys()))
+        siteDropdown.grid(row=1, column=1, sticky=NSEW, padx=4, pady=4)
+
+w = Window()
+w.window.mainloop()
