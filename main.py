f2176a5343849716c51f1126cbf468c25f20bf5f
Go1den <gooneden@gmail.com>
2020-07-12 02:55:51 -0500

    Huge progress on listbox functionality, stream previews, and actual dispatching to websites

--- main.py
+++ main.py
@@ -15,22 +15,5 @@ else:
     with open("oauth.txt", "r") as f:
         oAuth = f.read().rstrip()
 
-liveStreams = getLiveFollowedStreams(oAuth)
-
-w = Window(liveStreams)
+w = Window(oAuth)
 w.window.mainloop()
-
-# chosenStreams = easygui.multchoicebox(MSG_SELECT_STREAMS, TITLE_SELECT_STREAMS, liveStreams, None)
-# if chosenStreams is None:
-#     sys.exit(1)
-#
-# streamingSite = easygui.choicebox(MSG_SELECT_SITE, TITLE_SELECT_SITE, ORDERED_STREAMING_SITES.keys())
-# if streamingSite is None:
-#     sys.exit(1)
-
-# finalURL = ORDERED_STREAMING_SITES.get(streamingSite)
-# for chosenStream in chosenStreams:
-#     finalURL += chosenStream.split(" ")[0] + "/"
-#
-# webbrowser.open(finalURL, new=2)
-# sys.exit(0)
