7e307c807f08967309046e663bfdbc254e30d925
Go1den <gooneden@gmail.com>
2020-09-26 01:00:23 -0500

    Cleaning up that code yo

--- stream.py
+++ stream.py
@@ -1,11 +1,22 @@
+from sanitize import sanitize
+
 class Stream:
-    def __init__(self, gameTitle, previewImage, streamName, streamTitle, stylizedStreamName, viewerCount, boxArtURL):
-        self.gameTitle = gameTitle
-        self.previewImage = previewImage
-        self.streamName = streamName
-        self.streamTitle = streamTitle
-        self.stylizedStreamName = stylizedStreamName
-        self.viewerCount = viewerCount
+    def __init__(self, stream, gameTitle, boxArtURL):
+        self.ID = stream['id']
+        self.viewerCount = sanitize(str(stream['viewer_count']))
+        self.gameID = stream['game_id']
+        self.type = stream['type']
+        self.userID = stream['user_id']
+        self.language = stream['language']
+        self.thumbnailURL = sanitize(stream['thumbnail_url'])
+        self.stylizedStreamName = sanitize(stream['user_name'])
+        self.streamTitle = sanitize(stream['title'])
+        self.gameTitle = sanitize(gameTitle)
+        self.tagIDs = stream['tag_ids']
+        self.startedAt = stream['started_at']
+
+        self.previewImage = sanitize(stream['thumbnail_url'].replace('{width}', '320').replace('{height}', '180'))
+        self.streamName = sanitize(stream['thumbnail_url'][52:].split('-')[0])
         self.boxArtURL = boxArtURL
 
     def isLive(self, streams) -> bool:
